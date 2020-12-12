e6d257d0d371a224c37feeb9dc9b6fafb35c1032
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-12-12 20:47:49 +0100

    Already know everything :)

---
+++ variables.js
@@ -0,0 +1,11 @@
+/* It could be declared without "var" */
+/* Semicolon is not required, but a good practice */
+var name = "Abu";
+/* "Abu" = 'Abu' */
+
+var age = 27; //int
+var age2 = "27"; //string
+var pi = 3.14; // int (float)
+
+console.log(3.0 === 3);
+console.log(3 === 3);
